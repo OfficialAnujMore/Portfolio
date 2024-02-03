61af96cbec2853f6f08afb04792b8e8db1490184
Anuj More <moreanuj1307@gmail.com>
2024-02-03 12:42:09 +0530

    Under maintenance flag set

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import './App.css'
 
 function App(): React.ReactElement {
-    return <div className="App">Hello</div>
+    return <div className="App">Under maintenance</div>
 }
 
 export default App
