31a657b3afaea5bb9f69938b0de9a03a392793eb
Anuj More <moreanuj1307@gmail.com>
2024-02-25 18:27:04 +0530

    Update: Revamped landing screen with animation

--- src/modules/LandingSection.tsx
+++ src/modules/LandingSection.tsx
@@ -5,17 +5,23 @@ import { CustomHeading } from 'shared/components/CustomHeading'
 import { CustomDivider } from 'shared/components/CustomDivider'
 import { CustomButton } from 'shared/components/CustomButton'
 import NavigatorSection from './NavigatorSection'
+import CustomParticleContainer from 'shared/components/CustomParticleContainer'
 
 const LandingSection = (): React.ReactElement => {
     return (
         <div className={landingStyles.mainContainer}>
-            <CustomHeading text={enData.proflePrimaryTxt} />
+            <CustomHeading text={enData.proflePrimaryTxt} fontSize={'2rem'} />
             <CustomDivider customWdith="20%" />
-            <CustomHeading text={enData.profileName} fontSize={'3rem'} />
+            <CustomHeading text={enData.profileName} fontSize={'2.5rem'} />
             <CustomDivider customWdith="20%" />
-            <CustomHeading text={enData.profileTitle} fontSize={'3.5rem'} />
+            <CustomHeading text={enData.profileTitle} fontSize={'3rem'} />
+            <CustomHeading
+                text={enData.profileDescription}
+                fontSize={'1.5rem'}
+            />
             <CustomButton text={enData.resume} />
             <NavigatorSection />
+            <CustomParticleContainer />
         </div>
     )
 }
---
+++ src/shared/components/CustomParticleContainer.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react'
+import { Particles, initParticlesEngine } from '@tsparticles/react'
+import { loadSlim } from '@tsparticles/slim'
+const CustomParticleContainer = (): React.ReactElement => {
+    const [init, setInit] = useState(false)
+
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadSlim(engine)
+        }).then(() => {
+            setInit(true)
+        })
+    }, [])
+
+    return (
+        <div>
+            {init && (
+                <Particles
+                    id="tsparticles"
+                    options={{
+                        // background: {
+                        //     color: {
+                        //         value: '#ffffff',
+                        //     },
+                        //     opacity: 0.1,
+                        // },
+                        fpsLimit: 120,
+                        interactivity: {
+                            events: {
+                                onHover: {
+                                    enable: true,
+                                    mode: 'repulse',
+                                },
+                            },
+                            modes: {
+                                push: {
+                                    quantity: 100,
+                                },
+                                repulse: {
+                                    distance: 100,
+                                    duration: 0.1,
+                                },
+                            },
+                        },
+                        particles: {
+                            color: {
+                                value: '#ffffff',
+                            },
+                            links: {
+                                color: '#ffffff',
+                                distance: 150,
+                                enable: true,
+                                opacity: 0.2,
+                                width: 1,
+                            },
+                            move: {
+                                direction: 'none',
+                                enable: true,
+                                outModes: {
+                                    default: 'bounce',
+                                },
+                                random: false,
+                                speed: 0.3,
+                                straight: true,
+                            },
+                            number: {
+                                density: {
+                                    enable: true,
+                                },
+                                value: 200,
+                            },
+                            opacity: {
+                                value: 0.3,
+                            },
+                            shape: {
+                                type: 'circle',
+                            },
+                            size: {
+                                value: { min: 1, max: 4 },
+                            },
+                        },
+                        detectRetina: true,
+                    }}
+                />
+            )}
+        </div>
+    )
+}
+
+export default CustomParticleContainer
