16978da357a3cdcf2b53421e997abf55db40dadc
Anuj C More <moreanuj1307@gmail.com>
2025-05-07 12:01:25 -0700

    Initial code setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import React from 'react'
 import './App.css'
-import CustomNavbar from 'shared/components/CustomNavbar'
-import LandingSection from 'modules/LandingSection'
-import AboutMe from 'modules/AboutMe'
-import { Education } from 'modules/Education'
-import Experience from 'modules/Experience'
-import ContactForm from 'modules/ContactForm'
-import './styles/variables.css'
-import { Certifications } from 'modules/Certifications'
 
 function App(): React.ReactElement {
-    return (
-        <div className="App">
-            <CustomNavbar />
-            <LandingSection />
-            <AboutMe />
-            <Experience />
-            <Education />
-            <Certifications />
-            <ContactForm />
-        </div>
-    )
+    return <div className="App"></div>
 }
 
 export default App
