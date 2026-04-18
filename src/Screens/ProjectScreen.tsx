a67e76192e0d022d5f5826f81d7244fddbab8fab
Anuj C More <moreanuj1307@gmail.com>
2026-04-17 19:35:54 -0700

    Comment out unused project images and details in ProjectScreen.tsx

--- src/Screens/ProjectScreen.tsx
+++ src/Screens/ProjectScreen.tsx
@@ -7,9 +7,9 @@ import CustomText from '../Components/CustomText'
 import CustomButton from '../Components/CustomButton'
 import CustomImagePreview from '../Components/CustomImagePreview'
 import ProjectModal from '../Components/ProjectModal'
-import hirehourImg from '../assets/HireHour.png'
-import roadsideAssistanceImg from '../assets/RoadsideAssistance.png'
-import campusLinkImg from '../assets/CampusLink.png'
+// import hirehourImg from '../assets/HireHour.png'
+// import roadsideAssistanceImg from '../assets/RoadsideAssistance.png'
+// import campusLinkImg from '../assets/CampusLink.png'
 import kleanSQLImg1 from '../assets/KleanSQL.jpg'
 import kleanSQLImg2 from '../assets/KleanSQL2.jpg'
 import kleanSQLImg3 from '../assets/KleanSQL3.jpg'
@@ -219,116 +219,116 @@ const projects: Project[] = [
         category: 'AI & Web App',
         date: 'Oct 2025 - Oct 2025',
     },
-    {
-        id: 'hirehour',
-        title: 'HireHour – On-Demand Event Staffing Platform',
-        shortDescription:
-            'A real-time event staffing platform connecting users with nearby service providers for events.',
-        fullDescription: {
-            projectOverview:
-                "HireHour is a real-time event staffing platform that connects users with nearby service providers who can work at their events. Whether it's a corporate gathering, wedding, or community event, HireHour ensures that users can quickly find and hire the right professionals.",
-            keyContribution: [
-                'Developed a cross-platform mobile app using React Native & TypeScript, ensuring smooth performance on both iOS & Android.',
-                'Built a scalable backend with Node.js & PostgreSQL, optimizing database queries for faster response times.',
-                'Integrated location-based search, allowing users to find nearby service providers instantly.',
-                'Implemented real-time push notifications to keep users and service providers updated on job requests and confirmations.',
-                'Deployed and managed the application on DigitalOcean, ensuring high availability and performance.',
-                'Designed a secure authentication system with JWT, enhancing data privacy and user trust.',
-            ],
-            impact: [
-                'Reduced service booking time by enabling instant connections between users and service providers.',
-                'Increased job acceptance rate through automated notifications and an easy-to-use interface.',
-                'Enhanced platform reliability with optimized backend infrastructure and cloud deployment.',
-            ],
-        },
-        image: hirehourImg,
-        technologies: [
-            'React Native',
-            'TypeScript',
-            'JavaScript',
-            'Node.js',
-            'Prisma ORM',
-            'PostgreSQL',
-            'SendGrid',
-            'Push notification',
-            'Payment gateway integration',
-        ],
-        githubUrl: 'https://github.com/OfficialAnujMore/HireHour_FE',
-        projectUrl: '',
-        category: 'MOBILE APP',
-        date: 'Dec 2024 - Present',
-    },
-    {
-        id: 'roadside-assistance',
-        title: 'Roadside Assistance App – Emergency Vehicle Support',
-        shortDescription:
-            'A cross-platform app for instant vehicle breakdown support, featuring real-time location tracking and cloud-based notifications.',
-        fullDescription: {
-            projectOverview:
-                'The Roadside Assistance App is designed to provide instant support to users facing vehicle breakdowns by helping them find the nearest garage within a specified radius. The app ensures real-time location tracking and seamless communication with service providers, reducing wait times and improving roadside assistance efficiency.',
-            keyContribution: [
-                'Developed a cross-platform mobile app using React Native, ensuring smooth performance on both Android & iOS.',
-                'Implemented real-time location tracking via Google Maps API, enabling users to quickly locate nearby service providers.',
-                'Integrated Firebase for push notifications to instantly alert users and service providers about requests and status updates.',
-                'Built a scalable backend with Node.js, optimizing response times for fast service provider matching.',
-                'Designed a user-friendly UI that simplifies navigation and enhances the user experience.',
-            ],
-            impact: [
-                'Reduced assistance response time by efficiently connecting users with nearby garages.',
-                'Enhanced user experience with real-time tracking and automated notifications.',
-                'Improved reliability and performance by leveraging cloud-based infrastructure.',
-            ],
-        },
-        image: roadsideAssistanceImg,
-        technologies: [
-            'React Native',
-            'Firebase',
-            'Google Maps API',
-            'Node.js',
-            'MYSQL',
-        ],
-        githubUrl: 'https://github.com/OfficialAnujMore/Roadside-Assistance',
-        projectUrl: '',
-        category: 'MOBILE APP',
-        date: 'Aug 2020 - Dec 2020',
-    },
-    {
-        id: 'campus-link',
-        title: 'Campus Link - Bridging the Gap Between Parents & Institutions',
-        shortDescription:
-            'A mobile app for seamless communication between parents and educational institutions, with real-time data and notifications.',
-        fullDescription: {
-            projectOverview:
-                "Developed for the IT department of KJ Somaiya Institute of Engineering and Information Technology, this mobile application serves as a centralized platform for seamless communication between parents and the college. The app provides a user-friendly interface to track students' academic progress, attendance records, and upcoming events, fostering better parental engagement.",
-            keyContribution: [
-                'Designed and developed a cross-platform app using React Native, ensuring smooth performance on Android & iOS.',
-                'Implemented real-time data synchronization with Firebase, allowing parents to access updated student records.',
-                'Developed a secure backend with Node.js and PostgreSQL to manage student data efficiently.',
-                'Integrated push notifications to keep parents informed about attendance, exam schedules, and important announcements.',
-                'Designed a responsive and intuitive UI to enhance accessibility and ease of use.',
-            ],
-            impact: [
-                'Enhanced parental engagement by providing instant access to academic records.',
-                'Improved transparency between the institution and parents, strengthening communication.',
-                'Streamlined event notifications to ensure parents never miss crucial college updates.',
-            ],
-        },
-        image: campusLinkImg,
-        technologies: [
-            'React Native',
-            'Firebase',
-            'Node.js',
-            'PostgreSQL',
-            'Android Development',
-            'Android Studio',
-            'Android SDK',
-            'MySQL',
-        ],
-        githubUrl: '',
-        projectUrl: '',
-        category: 'MOBILE APP',
-        date: 'Jan 2020 - Feb 2020',
-    },
+    // {
+    //     id: 'hirehour',
+    //     title: 'HireHour – On-Demand Event Staffing Platform',
+    //     shortDescription:
+    //         'A real-time event staffing platform connecting users with nearby service providers for events.',
+    //     fullDescription: {
+    //         projectOverview:
+    //             "HireHour is a real-time event staffing platform that connects users with nearby service providers who can work at their events. Whether it's a corporate gathering, wedding, or community event, HireHour ensures that users can quickly find and hire the right professionals.",
+    //         keyContribution: [
+    //             'Developed a cross-platform mobile app using React Native & TypeScript, ensuring smooth performance on both iOS & Android.',
+    //             'Built a scalable backend with Node.js & PostgreSQL, optimizing database queries for faster response times.',
+    //             'Integrated location-based search, allowing users to find nearby service providers instantly.',
+    //             'Implemented real-time push notifications to keep users and service providers updated on job requests and confirmations.',
+    //             'Deployed and managed the application on DigitalOcean, ensuring high availability and performance.',
+    //             'Designed a secure authentication system with JWT, enhancing data privacy and user trust.',
+    //         ],
+    //         impact: [
+    //             'Reduced service booking time by enabling instant connections between users and service providers.',
+    //             'Increased job acceptance rate through automated notifications and an easy-to-use interface.',
+    //             'Enhanced platform reliability with optimized backend infrastructure and cloud deployment.',
+    //         ],
+    //     },
+    //     image: hirehourImg,
+    //     technologies: [
+    //         'React Native',
+    //         'TypeScript',
+    //         'JavaScript',
+    //         'Node.js',
+    //         'Prisma ORM',
+    //         'PostgreSQL',
+    //         'SendGrid',
+    //         'Push notification',
+    //         'Payment gateway integration',
+    //     ],
+    //     githubUrl: 'https://github.com/OfficialAnujMore/HireHour_FE',
+    //     projectUrl: '',
+    //     category: 'MOBILE APP',
+    //     date: 'Dec 2024 - Present',
+    // },
+    // {
+    //     id: 'roadside-assistance',
+    //     title: 'Roadside Assistance App – Emergency Vehicle Support',
+    //     shortDescription:
+    //         'A cross-platform app for instant vehicle breakdown support, featuring real-time location tracking and cloud-based notifications.',
+    //     fullDescription: {
+    //         projectOverview:
+    //             'The Roadside Assistance App is designed to provide instant support to users facing vehicle breakdowns by helping them find the nearest garage within a specified radius. The app ensures real-time location tracking and seamless communication with service providers, reducing wait times and improving roadside assistance efficiency.',
+    //         keyContribution: [
+    //             'Developed a cross-platform mobile app using React Native, ensuring smooth performance on both Android & iOS.',
+    //             'Implemented real-time location tracking via Google Maps API, enabling users to quickly locate nearby service providers.',
+    //             'Integrated Firebase for push notifications to instantly alert users and service providers about requests and status updates.',
+    //             'Built a scalable backend with Node.js, optimizing response times for fast service provider matching.',
+    //             'Designed a user-friendly UI that simplifies navigation and enhances the user experience.',
+    //         ],
+    //         impact: [
+    //             'Reduced assistance response time by efficiently connecting users with nearby garages.',
+    //             'Enhanced user experience with real-time tracking and automated notifications.',
+    //             'Improved reliability and performance by leveraging cloud-based infrastructure.',
+    //         ],
+    //     },
+    //     image: roadsideAssistanceImg,
+    //     technologies: [
+    //         'React Native',
+    //         'Firebase',
+    //         'Google Maps API',
+    //         'Node.js',
+    //         'MYSQL',
+    //     ],
+    //     githubUrl: 'https://github.com/OfficialAnujMore/Roadside-Assistance',
+    //     projectUrl: '',
+    //     category: 'MOBILE APP',
+    //     date: 'Aug 2020 - Dec 2020',
+    // },
+    // {
+    //     id: 'campus-link',
+    //     title: 'Campus Link - Bridging the Gap Between Parents & Institutions',
+    //     shortDescription:
+    //         'A mobile app for seamless communication between parents and educational institutions, with real-time data and notifications.',
+    //     fullDescription: {
+    //         projectOverview:
+    //             "Developed for the IT department of KJ Somaiya Institute of Engineering and Information Technology, this mobile application serves as a centralized platform for seamless communication between parents and the college. The app provides a user-friendly interface to track students' academic progress, attendance records, and upcoming events, fostering better parental engagement.",
+    //         keyContribution: [
+    //             'Designed and developed a cross-platform app using React Native, ensuring smooth performance on Android & iOS.',
+    //             'Implemented real-time data synchronization with Firebase, allowing parents to access updated student records.',
+    //             'Developed a secure backend with Node.js and PostgreSQL to manage student data efficiently.',
+    //             'Integrated push notifications to keep parents informed about attendance, exam schedules, and important announcements.',
+    //             'Designed a responsive and intuitive UI to enhance accessibility and ease of use.',
+    //         ],
+    //         impact: [
+    //             'Enhanced parental engagement by providing instant access to academic records.',
+    //             'Improved transparency between the institution and parents, strengthening communication.',
+    //             'Streamlined event notifications to ensure parents never miss crucial college updates.',
+    //         ],
+    //     },
+    //     image: campusLinkImg,
+    //     technologies: [
+    //         'React Native',
+    //         'Firebase',
+    //         'Node.js',
+    //         'PostgreSQL',
+    //         'Android Development',
+    //         'Android Studio',
+    //         'Android SDK',
+    //         'MySQL',
+    //     ],
+    //     githubUrl: '',
+    //     projectUrl: '',
+    //     category: 'MOBILE APP',
+    //     date: 'Jan 2020 - Feb 2020',
+    // },
 ]
 
 const ProjectCard: React.FC<{ project: Project; onClick: () => void }> = ({
